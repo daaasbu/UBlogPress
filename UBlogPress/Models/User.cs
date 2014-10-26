0e0062682ea0923e5855039625ad2ed9386843fe
Dana Asbury <daaasbu@indiana.edu>
2014-10-26 13:36:11 -0400

    update for classes

--- UBlogPress/Models/User.cs
+++ UBlogPress/Models/User.cs
@@ -7,17 +7,17 @@ namespace UBlogPress.Models
 {
     public class User
     {
-        public int Id { get; set; }
+        public int UserID { get; set; }
         public string Name_first { get; set; }
         public string Name_last { get; set; }
         public string Name_middle { get; set; }
         public string Email { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
-        public DateTime DT_created { get; set; }
-        public DateTime DT_updated { get; set; }
+        public DateTime DT_created_user { get; set; }
+        public DateTime DT_updated_user { get; set; }
         public string Birthday { get; set; }
-
+        
         public virtual Blog Blog { get; set; }
 
     }
