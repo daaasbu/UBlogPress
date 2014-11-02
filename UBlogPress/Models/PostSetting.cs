3168f5f41848e10f5adc630864de4d0b58d59a9f
Ryan Chibana <chibana.ryan@gmail.com>
2014-11-02 13:16:19 -0500

    Added PostSetting

---
+++ UBlogPress/Models/PostSetting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UBlogPress.Models
+{
+    public class PostSetting
+    {
+        public int IdPost { get; set; }
+
+        public string NamePostSetting { get; set; }
+
+        //make_post_setting()
+
+        //delete_post_setting()
+    }
+}
