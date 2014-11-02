594bec601faac4fb65c90d7a6bfdee14dc7c0a86
Ryan Chibana <chibana.ryan@gmail.com>
2014-11-02 13:09:52 -0500

    Added BlogSetting class

---
+++ UBlogPress/Models/BlogSetting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UBlogPress.Models
+{
+    public class BlogSetting
+    {
+        public int IdBlog { get; set; }
+
+        public string NmBlogSetting { get; set; }
+
+        public string DataBlogSetting { get; set; }
+    }
+}
