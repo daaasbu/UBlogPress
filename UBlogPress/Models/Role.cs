ac1c46b6ac5a9538b00c36887d9ceb3c5535fc7a
Ryan Chibana <chibana.ryan@gmail.com>
2014-11-02 13:15:00 -0500

    Added Role

---
+++ UBlogPress/Models/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UBlogPress.Models
+{
+    public class Role
+    {
+        public int IdRole { get; set; }
+
+        public string NameRold { get; set; }
+
+        //make_role()
+
+        //delete_role()
+    }
+}
