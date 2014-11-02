45b9ff0fe1795a7ef0836e9c672e380e75a32863
Ryan Chibana <chibana.ryan@gmail.com>
2014-11-02 13:19:11 -0500

    Filled in Post more

--- UBlogPress/Models/Post.cs
+++ UBlogPress/Models/Post.cs
@@ -10,15 +10,24 @@ namespace UBlogPress.Models
         public int PostID { get; set; }
         public string Title { get; set; }
         public string ContentPost { get; set; }
+        public string ExceptPost { get; set; }
         public Boolean Published { get; set; }
   
         public DateTime DTcreatedpost { get; set; }
         public DateTime DTupdatedpost { get; set; }
+        public DateTime DTAutoPublishPost { get; set; }
         public Boolean EnabledComment { get; set; }
-        public int UserID { get; set; }
+        public int BlogId { get; set; }
+        public int AuthorId { get; set; }
 
         public virtual User User { get; set; }
         public virtual ICollection<Post> Posts { get; set; }
         public virtual ICollection<Tag> Tags { get; set; }
+
+        //create_post()
+
+        //delete_post()
+
+        //save_draft()
     }
 }
