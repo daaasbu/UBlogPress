4ba9862c9870e16dce3c3a582331bd7ad26f089e
Ryan Chibana <chibana.ryan@gmail.com>
2014-11-30 12:30:35 -0500

    SearchUserModel test

---
+++ UBlogPressTests/TestSearchModels.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Dynamic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using UBlogPress.Models;
+using Assert = NUnit.Framework.Assert;
+
+
+namespace UBlogPress.Test
+{
+    [TestFixture]
+    public class TestSearchModels
+    {
+        private SearchUserModel _searchUser;
+
+        [SetUp]
+        public void Init()
+        {
+            _searchUser = new SearchUserModel()
+            {
+                NameDisplay = "Bigfoot",
+                Name = "Charles Foot",
+                DtCreated = new DateTime(1972,10,1)
+            };
+        }
+
+        [TestMethod]
+        public void SearchUserModelTest()
+        {
+            Assert.AreEqual(_searchUser.NameDisplay, "Bigfoot");
+            Assert.AreEqual(_searchUser.Name, "Charles Foot");
+            Assert.AreEqual(_searchUser.DtCreated, new DateTime(1972, 10, 1));
+        }
+    }
+}
