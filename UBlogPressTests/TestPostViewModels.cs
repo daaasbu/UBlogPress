3cbe8deb43da71c3b23fbe9e2358faaa6519842a
Ryan Chibana <chibana.ryan@gmail.com>
2014-11-30 12:25:21 -0500

    PostViewModels test

---
+++ UBlogPressTests/TestPostViewModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Dynamic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using UBlogPress.Models;
+using Assert = NUnit.Framework.Assert;
+
+
+namespace UBlogPress.Test
+{
+    [TestFixture]
+    public class TestPostViewModels
+    {
+        private PostViewModel _postViewModel;
+
+        [SetUp] 
+        public void Init()
+        {
+            _postViewModel = new PostViewModel()
+            {
+                Title = "title",
+                ContentPost = "somecontent",
+                Published = true,
+                EnabledComment = false
+            };
+        }
+
+        [TestMethod]
+        public void PostViewModelTest()
+        {
+            Assert.AreEqual(_postViewModel.Title, "title");
+            Assert.AreEqual(_postViewModel.ContentPost, "somecontent");
+            Assert.AreEqual(_postViewModel.Published, true);
+            Assert.AreEqual(_postViewModel.EnabledComment, true);
+        }
+    }
+}
