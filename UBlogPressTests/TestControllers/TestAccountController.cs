051594498b12d71a2db10b4c14fe39b18d30a223
Ryan Chibana <chibana.ryan@gmail.com>
2014-11-30 22:45:15 -0500

    Basic AccountController tests

--- UBlogPressTests/TestControllers/TestAccountController.cs
+++ UBlogPressTests/TestControllers/TestAccountController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using UBlogPress.Controllers;
+using UBlogPress.Models;
 using Assert = NUnit.Framework.Assert;
 
 namespace UBlogPress.Test.TestControllers
@@ -18,17 +22,39 @@ namespace UBlogPress.Test.TestControllers
         }
 
         [Test]
-        public void TestLoginSuccess()
+        public void TestRegisterLoginSuccess()
         {
-            
+            var register = new RegisterViewModel()
+            {
+                Email = "psh@gmail.com",
+                NameDisplay = "Phil",
+                NameBlog = "lifehacker",
+                Password = "passwd",
+                ConfirmPassword = "passwd",
+                NameFirst = "Philip",
+                NameMiddle = "Seymour",
+                NameLast = "Hoffman",
+                Birthday = new DateTime(1991, 7, 21)
+            };
+            Task<ActionResult> task = _accountController.Register(register);
+
+            var login = new LoginViewModel()
+            {
+                Email = "psh@gmail.com",
+                Password = "passwd",
+                RememberMe = false
+            };
+            Task<ActionResult> result = _accountController.Login(login, "returnUrl.com");
+   
+            Assert.IsNotNull(result, "Action result is null");
+            Assert.IsInstanceOf(typeof (Task<ActionResult>), result, "Action result of wrong type");
         }
 
         [Test]
-        public void TestLoginFailure()
+        public void TestLoginSimple()
         {
-            
+            var result = _accountController.Login("google.com") as ViewResult;
+            Assert.IsInstanceOf(typeof(ViewResult), result);
         }
-       
-    
     }
 }
